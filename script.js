b557e2e0080b4eaae72bbd8130ca1e532f53c9dd
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-05-13 23:40:22 -0300

    meio

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 
-function contar(){
-    console.log('hi amanda')
+function contar(){ 
+    
+    //recupera os valores dos elementos no HTML, Number converte string para número
+    var nInicial = Number(document.getElementById('inicio').value)
+    var nFinal = Number(document.getElementById('fim').value)
+    var pulada = Number(document.getElementById('passo').value)
+    var resultado = []
+
+
+
+     if (nInicial >= nFinal){ //se o nfinal for menor que o ninicial 
+            
+        console.log('O início da contagem é maior que o número final, escolha outro.')
+    } else {
+        while(nInicial <= nFinal ){//loop para fazer a contagem, termina quando nfinal é menor que o nInicial
+            
+            resultado.push(nInicial)
+            nInicial += pulada
+        }
+            
+        console.log(`${resultado.join(' ')}`)
+        console.log(`O resultado final é ${nInicial-pulada}`)
+
+}
 }
