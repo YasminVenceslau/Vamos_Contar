62e4da81c40361a612739c4e5f613bcef64b6a45
YasminVenceslau <yasmin.venceslau@hotmail.com>
2024-05-19 20:42:18 -0300

    pronto

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 
-function contar(){ 
     
+    
+    
+function contar(){ 
     //recupera os valores dos elementos no HTML, Number converte string para número
     var nInicial = Number(document.getElementById('inicio').value)
     var nFinal = Number(document.getElementById('fim').value)
     var pulada = Number(document.getElementById('passo').value)
+    var res = document.getElementById('resultado')
+    
+    var falta = "Escolha outros valores!"
+
     var resultado = []
+    
+    var spanTem = "<span class='tem'></span>"
+    var spanNTem ="<span class='ntem'> Parâmetros Inválidos</span>"
+    
+    
+     if (nInicial >= nFinal){ //se o nfinal for menor que o nInicial
+        
 
-
-
-     if (nInicial >= nFinal){ //se o nfinal for menor que o ninicial 
-            
-        console.log('O início da contagem é maior que o número final, escolha outro.')
+        res.innerHTML =  spanNTem + "<br>" + falta
+        
     } else {
-        while(nInicial <= nFinal ){//loop para fazer a contagem, termina quando nfinal é menor que o nInicial
+        
+        for (let i = nInicial ; i <= nFinal;i += pulada){
+        
+            resultado.push(i)
             
-            resultado.push(nInicial)
-            nInicial += pulada
+
         }
-            
-        console.log(`${resultado.join(' ')}`)
-        console.log(`O resultado final é ${nInicial-pulada}`)
+        let certo = resultado.map( t=> t.toString().match(/.{1,6}/g).join('👉'))
+    
+        res.innerHTML = certo.join('👉') + spanTem
+    }
+
+    
+    
+    
+   
 
 }
-}
+
